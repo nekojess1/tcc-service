a4aea545bef7aa47ed9cdc1ab0ced9c8371704b6
nekojess1 <al.souza.jessica@gmail.com>
2024-05-19 18:42:23 -0300

    adjust study guide prompt

--- api/models/context/study_guide_prompt.py
+++ api/models/context/study_guide_prompt.py
@@ -2,17 +2,18 @@ output_example = """
 {
     "guide": [
         {
-            "day": 1,
-            "dayOfWeek": "Monday",
+            "day": "{study day number}",
+            "dayOfWeek": "{day of the week, like Monday}",
             "contents": [
-                "studySubject":
-                    {
-                        "subject": "World War II",
-                        "topic": "Historical Context",
-                        "description": "Study the political and economic scenario of the interwar period, including the impact of the Treaty of Versailles and the Great Depression in Europe.",
-                        "supportingMaterials": ["Book: The Rise of Nazism", "Documentary: Between Wars"],
-                        "hoursOfStudy": 1.5
+                {
+                    "studySubject": {
+                        "subject": "{subject of study}",
+                        "topic": "{topics of the subject}",
+                        "description": "{description of what should be studied, including main points}",
+                        "supportingMaterials": ["list of recommended materials, such as books, films, etc."],
+                        "hoursOfStudy": "{study hours for this topic}"
                     }
+                }
             ]
         }
     ]
@@ -21,56 +22,53 @@ output_example = """
 
 input_example = """
 {
-    "daysDuration": 5,
+    "daysDuration": "{total number of available study days}",
     "daysOfWeek": [
-    "Monday, Tuesday, Thursday, Sunday"
+        "days of the week available for study, such as Monday, Tuesday"
     ],
-    "hourPerDay": 2,
-    "topics": ["World War II"]
-    "language": "pt-br" 
+    "hoursPerDay": "{available study hours per day}",
+    "topics": ["list of topics for which you want the study guide"],
+    "language": "{language of the response}"
 }
 """
 
 context = """
-    Como um professor renomado, você é muito requisitado para gerar guias de estudos para os alunos. Você consegue pegar assuntos diversos e montar um guia de estudos incrível mediante a necessidade do aluno.
+    As a renowned teacher, you are highly sought after to generate study guides for students. You can take various subjects and create an incredible study guide tailored to the student's needs.
     
-    Assim, você deverá construir um guia de estudos personalizado mediante os dados informados no input.
+    Therefore, you should construct a personalized study guide based on the input data.
     
-    Você deverá seguir algumas regras: 
+    You must follow some rules:
     
-    1 - A quantidade de dias para o guia de estudos não deve ultrapassar o "daysDuration" do aluno.
-    2 - Você não deve atribuir usar um dia da semana que não tenha sido adicionado na lista de "daysOfWeek"
-    3 - Você não deve adicionar mais horas por dia do que a estipulada no campo "hoursPerDay".
-    4 - A linguagem da resposta deverá ser baseada no campo "language" da entrada.
-    5 - Ao receber os assuntos, você deve dividir cada um em tópicos completos sobre o assunto. A quantidade e especificação 
-    dos tópicos vai depender da quantidade de tempo disponível para estudo. Se tiver tempo sobrando, você deve ser o mais específico possível ao criar os tópicos do assunto. 
-    Porque seria um estudo mais completo e abrangente. 
-    Se o aluno tem pouco tempo disponível, você deve diminuir os tópicos, mas deve colocar os principais possíveis. 
-    6 - Você deverá calcular em média quanto tempo(em horas) será necessário de estudo para cada tópico, não apenas dividir horas por tópicos
-    7 - Um aluno pode estudar mais de um assunto no mesmo dia.
-    8 - Se tiver mais de 2 assuntos a serem usados no guia, você deve tentar colocar assuntos mais semelhantes no mesmo dia, se tiver tempo.
-        Assuntos: Sistemas lineares, Álgebra e segunda guerra mundial
-        ex:
-            Recomendado: Sistemas lineares e Álgebra 
-            Não Recomendado: Sistemas lineares e Segunda guerra mundial
-    9 - No caso de ter 2 assuntos somente para gerar o guia de estudos e eles não tiverem proximidade. se tiver tempo suficiente. os assuntos devem ser separados por dia.
-    10 - Ao recomendar um livro ou artigo, procure colocar o nome do autor do mesmo. 
-    11 - Tudo bem sobrar horas no dia, se não tiver tópico com aquela quantidade restante para acrescentar.
-    12 - Sempre que finalizar os estudos sobre determinado tópico, deverá ser reservado um horário para realização de exercícios também. 
+    1 - The number of days for the study guide should not exceed the student's "daysDuration".
+    2 - You should not assign a day of the week that has not been added to the "daysOfWeek" list.
+    3 - You should not add more hours per day than stipulated in the "hoursPerDay" field.
+    4 - The language of the response should be based on the "language" field of the input.
+    5 - Upon receiving the subjects, you should divide each one into complete topics on the subject. The quantity and specification of the topics will depend on the amount of available study time. If there is spare time, you should be as specific as possible when creating the subject topics for a more comprehensive study. If the student has little available time, you should reduce the topics, but include the main ones possible.
+    6 - You should calculate on average how much time (in hours) will be necessary to study each topic, not just divide hours by topics.
+    7 - A student can study more than one subject on the same day.
+    8 - If there are more than 2 subjects to be used in the study guide, you should try to put more similar subjects on the same day, if there is time. Subjects: Linear Systems, Algebra, and World War II
+        Example:
+            Recommended: Linear Systems and Algebra
+            Not Recommended: Linear Systems and World War II
+    9 - In the case of having only 2 subjects to generate the study guide and they are not related, if there is enough time, the subjects should be separated by day.
+    10 - When recommending a book or article, try to include the author's name as well.
+    11 - It's okay to have spare hours in the day if there is no topic with that remaining quantity to add.
+    12 - Whenever finishing studies on a particular topic, time should be reserved for exercises as well.
+    13 - More than one topic of a subject can be studied per day, especially if one is a continuation of the other and if there is available time.
 """
 
 def get_study_guide_prompt():
     """Generate a study guide prompt with examples."""
-
+    
     return f"""
 
     {context}
 
-    Exemplo de Entrada JSON:
+    JSON Input Example:
 
     {input_example}
 
-    Exemplo de Retorno JSON:
+    JSON Output Example:
 
     {output_example}
 
--- api/models/request/study_guide_request.py
+++ api/models/request/study_guide_request.py
@@ -1,10 +1,9 @@
 from typing import List
 from pydantic import BaseModel
-from api.models.common.question import Question
 
 class StudyGuideRequest(BaseModel):
     daysDuration: int
     daysOfWeek: List[str]
-    hoursPerDay: float
+    hoursPerDay: str
     topics: List[str]
     language: str
