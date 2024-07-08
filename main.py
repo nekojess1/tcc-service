75c5c1317925cead0a3181762c290e289dba82aa
nekojess1 <al.souza.jessica@gmail.com>
2024-07-08 20:37:58 -0300

    adjust open ai key config

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from api.routes.gpt_routes import router as gpt_router
+import openai
+from api.config.settings import settings
 
 # Carregar variáveis de ambiente
 load_dotenv()
+
+# Configurar a chave da API OpenAI 
+openai.api_key = settings.openai_api_key
 
 app = FastAPI()
 
