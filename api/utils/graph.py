4e200d304f6abe9a25ce37bdcde2f094ae97b7a1
nekojess1 <al.souza.jessica@gmail.com>
2024-09-09 12:25:31 -0300

    add color error to mind map

--- api/utils/graph.py
+++ api/utils/graph.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 class Node(BaseModel):
     id: str
     description: str
-    is_common_error: bool
+    hit_percentage: int
     color: Optional[str] = "darkolivegreen3"
     shape: Optional[str] = "box"
     style: Optional[str] = "solid"
@@ -24,16 +24,17 @@ class GraphStructure(BaseModel):
     
 
 def get_graph(graph_structure: GraphStructure) -> Digraph:
-    dot = Digraph(comment='Mapa Mental', engine='circo')
+    dot = Digraph(comment='Mapa Mental', engine='fdp')
+
     for node in graph_structure.nodes:
-        dot.node(node.id, node.description, color = get_error_color(node.is_common_error), shape=node.shape, style=node.style)
+        dot.node(node.id, node.description, color = get_error_color(node.hit_percentage), shape=node.shape, style=node.style)
     for edge in graph_structure.edges:
         dot.edge(edge.from_node, edge.to_node, color=edge.color, style=edge.style)
     print(dot)
     return dot
 
-def get_error_color(isCommonError: bool):
-    if isCommonError:
+def get_error_color(hit_percentage: int):
+    if hit_percentage != 100:
         return "indianred"
     else:
         return "darkolivegreen3"
@@ -41,9 +42,10 @@ def get_error_color(isCommonError: bool):
 def generate_mind_map(graph_structure: GraphStructure):
     dot = get_graph(graph_structure)
     img_stream = io.BytesIO()
-    img_stream.write(dot.pipe(format='png'))
+    img_stream.write(dot.pipe(format='pdf'))
     img_stream.seek(0)
-    with open("test_mind_map.png", "wb") as f:
+    with open("test_mind_map.pdf", "wb") as f:
         f.write(img_stream.read())
     img_stream.seek(0)  # Reset the stream position for reading in Response
-    return Response(content=img_stream.read(), media_type="image/png")
+    return Response(content=img_stream.read(), media_type="application/pdf")
+
