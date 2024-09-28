e7f715f14a6c08910f1b953ad4f826aa5ac54156
nekojess1 <al.souza.jessica@gmail.com>
2024-09-28 11:25:33 -0300

    remove study guide service

--- api/models/requests/study_guide_request.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from pydantic import BaseModel
-
-class StudyGuideRequest(BaseModel):
-    daysDuration: int
-    daysOfWeek: List[str]
-    hoursPerDay: str
-    topics: List[str]
-    language: str
