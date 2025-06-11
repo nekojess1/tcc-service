1d5bd76575ae21542e5ad2b020bb2eee88185db6
nekojess1 <al.souza.jessica@gmail.com>
2025-06-10 22:59:09 -0300

    remove unnecessary imports and fill validate_exercises_prompt

--- api/models/prompts/exercises/validate_exercises/validate_exercises_prompt_elementary_students.py
+++ api/models/prompts/exercises/validate_exercises/validate_exercises_prompt_elementary_students.py
@@ -1,8 +1,3 @@
-from api.models.prompts.exercises.validate_exercises.validate_exercises_examples import input_format, output_format, output_example, input_example
-from api.models.prompts.exercises.validate_exercises.validate_exercises_instructions import context as generic_context
-from api.models.prompts.exercises.validate_exercises.validate_exercises_instructions_adjusted import context as guided_context
-
-
 def get_validate_elementary_students_exercises_prompt():
     return """
 # Contexto da Simulação
