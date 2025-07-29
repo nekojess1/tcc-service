00b372743e7e468e28a32e1b02c433239968e9f1
nekojess1 <al.souza.jessica@gmail.com>
2025-07-29 20:59:00 -0300

    remove unnecessary files and comments

--- api/models/responses/exercise_response.py
+++ api/models/responses/exercise_response.py
@@ -4,12 +4,9 @@ from typing import List, Optional
 class Question(BaseModel):
     type: str
     question: str
-    options: Optional[List[str]] = None  # Opcional, pois nem todos os tipos de perguntas têm opções
+    options: Optional[List[str]] = None  
     answer: str
 
-class Exercise(BaseModel):
+class ExercisesResponse(BaseModel):
     topic: str
     questions: List[Question]
-
-class ExercisesResponse(BaseModel):
-    exercises: List[Exercise]
